51a2f94621a14d7063590497a6698a925f5220c9
Tej Chajed <tchajed+github@gmail.com>
2014-09-10 13:33:46 -0400

    Initial commit with completed tutorial

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+
+	public float speed = 500.0f;
+	public Text countText;
+	public Text winText;
+
+	private int count;
+
+	private void UpdateText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count < 12) {
+			winText.text = "";
+		} else {
+			winText.text = "You Win!";
+		}
+	}
+
+	void Start() {
+		count = 0;
+		UpdateText();
+	}
+
+	// Called before physics calculations.
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("PickUp")) {
+			other.gameObject.SetActive(false);
+			count++;
+			UpdateText();
+		}
+	}
+}
